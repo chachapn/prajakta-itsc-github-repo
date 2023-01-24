3b3576aeb847bc050b8b35d4824b7210616b4aef
chachapn <761+chachapn@users.noreply.git.uc.edu>
2023-01-24 15:34:53 -0500

    ocat-task2-assignment-finished

--- packages/api/src/microservices/Assessment-Service.js
+++ packages/api/src/microservices/Assessment-Service.js
@@ -11,15 +11,14 @@ exports.submit = async (assessment) => {
 
   });
 
-  global.console.log(assessment);
   // use the sequelize model Assessments from packages/api/src/database/models to save
   // the assessment data in the PostgreSQL database
 };
 
-exports.getList = () => {
+exports.getList = async () => {
+
   // use the sequelize model Assessments from packages/api/src/database/models to fetch
   // the assessment data from the PostgreSQL database
-  const assessments = [];
-
+  const assessments = await Assessment.findAll();
   return assessments;
 };
